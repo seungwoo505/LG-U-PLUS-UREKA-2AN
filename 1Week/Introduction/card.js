40f0a330b7fd2d3beee9761de63a5434ee3db816
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2025-01-28 18:43:56 +0900

    card 마우스 이벤트 추가
    3d 액션 추가

--- 1Week/Introduction/card.js
+++ 1Week/Introduction/card.js
@@ -1,9 +1,29 @@
 document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
+    card.addEventListener('click', (e) => {
+      const x = e.offsetX;
+      const y = e.offsetY;
+
+      
       card.classList.toggle('flip');
     });
+
+    card.addEventListener('mousemove', (e) => {
+      let x = e.offsetX;
+      let y = e.offsetY;
+
+      let rotateY = -40/card.offsetWidth * x + 20 + (card.classList[1] === "flip" ? 180 : 0);
+      let rotateX = 40/card.offsetHeight * y - 20;
+
+      card.style = `transform : rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+
+
+    card.addEventListener('mouseleave', () => {
+      card.style.transform = `rotateX(0deg) rotateY(${card.classList[1] === "flip" ? 180 : 0}deg)`;
+    });
+    
   });
   
   document.getElementById('link').addEventListener('click', (e) =>{
     e.stopPropagation();
-  })
+  });
