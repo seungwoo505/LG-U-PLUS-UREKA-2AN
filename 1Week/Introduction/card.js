c614059709d53c2ae208b6e5e7d34dbe2ee2ab2f
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2025-01-30 10:21:01 +0900

    코드 최적화 및 클릭 가능 범위 증가

--- 1Week/Introduction/card.js
+++ 1Week/Introduction/card.js
@@ -1,10 +1,13 @@
+let x_row = 0;
+let y_row = 0;
+
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', (e) => {
       const x = e.offsetX - (card.offsetWidth/2);
       const y = (card.offsetHeight/2) - e.offsetY;
 
-      const y_max = card.offsetHeight * 0.1;
-      const x_max = card.offsetWidth * 0.1;
+      const y_max = card.offsetHeight * 0.3;
+      const x_max = card.offsetWidth * 0.3;
 
       const top = (card.offsetHeight/2) - y_max;
       const right = (card.offsetWidth/2) - x_max;
@@ -12,26 +15,32 @@ document.querySelectorAll('.card').forEach(card => {
       const left = x_max - (card.offsetWidth/2);
 
       if(card.classList.length > 1){
+        x_row = 0;
+        y_row = 0;
         card.className = "card";
-        card.style = `transform : rotateX(0deg)`;
+        card.style = `transform : rotateX(${x_row}deg) rotateY(${y_row}deg)`;
       }
       else if(y > top){
         document.getElementsByClassName("card-back")[0].id = "top";
-        card.style = `transform : rotateX(-180deg)`;
+        x_row -= 180;
+        card.style = `transform : rotateX(${x_row}deg) rotateY(${y_row}deg)`;
         card.className +=  ' top';
       }else if(y < button){
         document.getElementsByClassName("card-back")[0].id = "button";
-        card.style = `transform : rotateX(180deg)`;
+        x_row += 180;
+        card.style = `transform : rotateX(${x_row}deg) rotateY(${y_row}deg)`;
         card.className +=  ' button';
       }
       else if(x > right){
         document.getElementsByClassName("card-back")[0].id = "right";
-        card.style = `transform : rotateX(0deg) rotateY(-180deg)`;
+        y_row -= 180;
+        card.style = `transform : rotateX(${x_row}deg) rotateY(${y_row}deg)`;
         card.className +=  ' right';
       }
       else if(x < left){
         document.getElementsByClassName("card-back")[0].id = "left";
-        card.style = `transform : rotateX(0deg) rotateY(180deg)`;
+        y_row += 180;
+        card.style = `transform : rotateX(${x_row}deg) rotateY(${y_row}deg)`;
         card.className +=  ' left';
       }
     });
@@ -40,15 +49,15 @@ document.querySelectorAll('.card').forEach(card => {
       const x = e.offsetX;
       const y = e.offsetY;
 
-      const rotateY = (card.classList[1] === "top" || card.classList[1] === "button" ? -1 : 1) * (-40/card.offsetWidth * x + 20 + (card.classList[1] === "right" ? -180 : card.classList[1] === "left" ? 180 : 0));
-      const rotateX = 40/card.offsetHeight * y - 20 + (card.classList[1] === "top" ? -180 : card.classList[1] === "button" ? 180 : 0);
+      const rotateY = (card.classList[1] === "top" || card.classList[1] === "button" ? -1 : 1) * (-40/card.offsetWidth * x + 20 + y_row);
+      const rotateX = 40/card.offsetHeight * y - 20 + x_row;
       
       card.style = `transform : rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
     });
 
 
     card.addEventListener('mouseleave', () => {
-      card.style.transform = `rotateX(${card.classList[1] === "top" ? -180 : card.classList[1] === "button" ? 180 : 0}deg) rotateY(${card.classList[1] === "right" ? -180 : card.classList[1] === "left" ? 180 : 0}deg)`;
+      card.style.transform = `rotateX(${x_row}deg) rotateY(${y_row}deg)`;
     });
     
   });
