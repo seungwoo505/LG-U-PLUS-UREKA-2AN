da2c0f93f74fb50b4076c1fd0cbb0c28546aa2a8
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2025-02-03 18:41:19 +0900

    스프링 맛보기
    간단 spring

---
+++ 2Week/6Day/java/back02_web/src/main/java/com/j02/hello/MainController.java
@@ -0,0 +1,36 @@
+package com.j02.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	/*
+	@GetMapping("/")
+	public String index() {
+		return "<!DOCTYPE html>\r\n"
+				+ "<html lang=\"ko\">\r\n"
+				+ "<head>\r\n"
+				+ "    <meta charset=\"UTF-8\">\r\n"
+				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+				+ "    <title>Todo List</title>\r\n"			
+				+ "</head>\r\n"
+				+ "<body>\r\n"
+				+ "\r\n"
+				+ "<div id=\"todoApp\">\r\n"
+				+ "    <h1>Todo List</h1>\r\n"
+				+ "    <input type=\"text\" id=\"todoInput\" placeholder=\"할 일을 입력하세요\">\r\n"
+				+ "    <button id=\"addButton\">등록</button>\r\n"
+				+ "    <ul id=\"todoList\">\r\n"
+				+ "        <li>예시 할 일 1</li>\r\n"
+				+ "        <li>예시 할 일 2</li>\r\n"
+				+ "    </ul>\r\n"
+				+ "</div>\r\n"
+				+ "\r\n"
+				+ "</body>\r\n"
+				+ "</html>\r\n"
+				+ "";
+	}
+	*/
+}
---
+++ 2Week/6Day/java/back02_web/src/test/java/com/j02/hello/Back02WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j02.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back02WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
