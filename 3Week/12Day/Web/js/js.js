a30c943d1b07928e1d6d8d0bde24431fed2b6749
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2025-02-11 23:45:29 +0900

    12일차 과제
    서버 및 css 추가

--- 3Week/12Day/Web/js/js.js
+++ 3Week/12Day/Web/js/js.js
@@ -1,14 +1,64 @@
 const login = () =>{
-    let id = document.getElementById("id").innerHTML;
-    let pw = document.getElementById("pw").innerHTML;
-
+    const id = document.getElementById("id").value;
+    const pw = document.getElementById("pw").value;
+    
     fetch("https://seungwoo.i234.me:3333/login", {
         method : "POST",
+        headers : { "Content-Type" : "application/json"},
         body : JSON.stringify({
             userId : id,
             password : pw
         })
     })
-    .then(e => console.log(e))
-    .then(e => console.error(e));
+    .then( async e =>{
+        const data = await e.json();
+        alert(data.message);
+        if(data.success)document.getElementById("login").innerHTML = `${data.nickname}님 환영합니다.<button onClick="logout()">로그아웃</button>`;
+    });
+}
+
+const logout = () => {
+    document.getElementById("login").innerHTML = `id : <input id="id" autocomplete="username" type="text" name="id"><br>
+        password : <input id="pw" autocomplete="current-password" type="password" name="password"><br>
+        <button onclick="login()">로그인</button>`;
+}
+
+const book = () => {
+    const book = document.getElementById("book").value;
+
+    fetch("https://seungwoo.i234.me:3333/bookSearch", {
+        method : "POST",
+        headers : { "Content-Type" : "application/json"},
+        body : JSON.stringify({
+            book : book
+        })
+    })
+    .then( async e =>{
+        const data = await e.json();
+        alert(data.message);
+        alert(`제목 : ${data.book[0]}\n지은이 : ${data.book[1]}\n줄거리 : ${data.book[2]}`);
+    });
+}
+
+const bookAdd = () => {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const plot = document.getElementById("plot").value;
+
+    fetch("https://seungwoo.i234.me:3333/bookAdd", {
+        method : "POST",
+        headers : { "Content-Type" : "application/json"},
+        body : JSON.stringify({
+            title : title,
+            author : author,
+            plot : plot
+        })
+    })
+    .then( async e =>{
+        const data = await e.json();
+        alert(data.message);
+        document.getElementById("title").value = "";
+        document.getElementById("author").value = "";
+        document.getElementById("plot").value = "";
+    });
 }
---
+++ 3Week/12Day/Web/js/server.js
@@ -0,0 +1,101 @@
+const fs = require("fs");
+const https = require("https");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const mariaDB = require("mysql2/promise");
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+//https 인증서 위치
+const options = {
+    key : fs.readFileSync(process.env.HTTPS_KEY),
+    cert : fs.readFileSync(process.env.HTTPS_CERT),
+    ca : fs.readFileSync(process.env.HTTPS_CA),
+};
+
+const db = mariaDB.createPool({
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_SCHEMA
+});
+
+app.post("/login", async (req, res) => {
+    const { userId, password } = req.body;
+
+    try {
+        const [rows] = await db.query("SELECT * FROM SHOPPING_USER_INFORMATION WHERE USER_ID = ? AND USR_YN = 1", [userId]);
+        if(rows.length === 0) {
+            console.log("사용자를 찾지못했습니다.");
+            return res.status(404).json({success : false, message : "사용자를 찾을 수 없습니다."});
+        }
+
+        const user = rows[0];
+
+        if(user.FAIL_COUNT >= 5) {
+            console.log("비밀번호 5회 초과");
+            return res.status(401).json({success : false, message : "비밀번호를 5회 이상 틀리셨습니다.\n 비밀번호 변경 후 이용해주시길 바랍니다."});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.PASSWORD);
+
+        if(!isPasswordValid) {
+            console.log("패스워드 오류");
+            await db.query("UPDATE SHOPPING_USER_INFORMATION SET FAIL_COUNT = ? WHERE USER_ID = ?", [user.FAIL_COUNT + 1, userId]);
+            return res.status(401).json({success : false, message : "아이디나 비밀번호가 일치하지않습니다."});
+        }
+
+        await db.query("UPDATE SHOPPING_USER_INFORMATION SET FAIL_COUNT = ? WHERE USER_ID = ?", [0, userId]);
+
+        console.log(`${userId} 로그인`);
+
+        return res.status(200).json({success : true, message : `${userId}님 환영합니다.`, nickname : user.NICKNAME});
+
+
+    }catch (err){
+        console.error(err);
+        return res.status(500).json({success : false, message : "로그인이 실패했습니다.\n잠시 후에 다시 시도해주세요."});
+    }
+});
+
+app.post("/bookSearch", async (req, res) => {
+    const { book } = req.body;
+
+    try{
+        const [rows] = await db.query("SELECT * FROM BOOK WHERE TITLE = ? AND BOOK_YN = 1", [book]);
+
+        if(rows.length === 0){
+            console.log("책을 찾지 못했습니다.");
+            return res.status(404).json({success : false, message : "책을 찾을 수 없습니다."});
+        }
+
+        console.log("책을 찾았습니다.");
+        return res.status(200).json({success : true, message : `${book}과 연관된 책은 ${rows.length} 권 있습니다.`, book : [rows[0].TITLE, rows[0].AUTHOR, rows[0].PLOT]});
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({success : false, message : "조회에 실패했습니다.\n잠시 후에 다시 시도해주세요."});
+    }
+});
+
+app.post("/bookAdd", async (req, res) => {
+    const { title, author, plot } = req.body;
+
+    try{
+        await db.query("INSERT INTO BOOK(TITLE, AUTHOR, PLOT, BOOK_YN) VALUES(?, ?, ?, 1)", [title, author, plot]);
+
+        console.log("책을 넣었습니다.");
+        return res.status(200).json({success : true, message : `${title}이 등록되었습니다.`});
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({success : false, message : "등록에 실패했습니다.\n잠시 후에 다시 시도해주세요."});
+    }
+});
+
+https.createServer(options, app).listen(3333, () => console.log("서버가 연결되었습니다."));
