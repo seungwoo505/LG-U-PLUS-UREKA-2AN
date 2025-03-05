59f06e6045aa14e64a5a2127ade3f54ef268cedf
seungwoo <zoozoo1302@gmail.com>
2025-03-05 19:30:03 +0900

    26일차 추가 공부

--- 6Week/26Day/project1/front/js/index.js
+++ 6Week/26Day/project1/front/js/index.js
@@ -39,9 +39,26 @@ document.getElementById("signupBtn").addEventListener("click", async () => {
     const modal = bootstrap.Modal.getInstance(document.getElementById("signupModal"));
     modal.hide();
     //hero icons
-    document.getElementById("loginSpan").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" width="24" height="24">
+    /* document.getElementById("loginSpan").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" width="24" height="24">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
-</svg>`;
+</svg>`; */
     document.getElementById("signupLi").remove();
   }
+});
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+  const email = document.getElementById("loginEmail").value;
+  const pw = document.getElementById("loginPw").value;
+  const data = { email, pw };
+  let response = await fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data),
+  });
+  response = await response.json();
+  console.log(response.msg);
+  alert(response.msg);
 });
