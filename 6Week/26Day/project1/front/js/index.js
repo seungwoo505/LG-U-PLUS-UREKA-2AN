84a1704520c2991555006949795529a3914ce6f5
seungwoo <zoozoo1302@gmail.com>
2025-03-07 08:55:06 +0900

    28일차 공부

--- 6Week/26Day/project1/front/js/index.js
+++ 6Week/26Day/project1/front/js/index.js
@@ -1,4 +1,10 @@
 window.onload =  async () => {
+  const email = sessionStorage.getItem("email");
+  if(email !== null){
+    document.getElementById("loginSpan").innerHTML = email + `<button id = "logoutBtn" class="btn btn-outline-danger">로그아웃</button>`;
+  }
+  axios.defaults.withCredentials = true;
+  console.log(axios);
   let responseObj = await fetch("http://localhost:8080/getAllProducts", {method : "GET"});
   let arr = await responseObj.json();
 
@@ -10,8 +16,8 @@ window.onload =  async () => {
     <img src="./img/${element.pimg}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${element.prodname}</h5>
-      <p class="card-text">${element.price}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <p class="card-text">${element.price}원</p>
+      <a href="#" class="btn btn-outline-info" id="addCart">장바구니 담기</a>
     </div>
   </div>`;
   });
@@ -50,15 +56,31 @@ document.getElementById("loginBtn").addEventListener("click", async () => {
   const email = document.getElementById("loginEmail").value;
   const pw = document.getElementById("loginPw").value;
   const data = { email, pw };
-  let response = await fetch("http://localhost:8080/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(data),
-  });
-  response = await response.json();
-  console.log(response.msg);
-  alert(response.msg);
+  let response = await axios.post("http://localhost:8080/login", data);
+  // response = await response.json();
+  console.log(response.data.msg);
+  alert(response.data.msg);
+  if(response.data.msg === "ok"){
+    const modal = bootstrap.Modal.getInstance(document.getElementById("loginModal"));
+    modal.hide();
+
+    document.getElementById("loginSpan").innerHTML = email + `<button id = "logoutBtn" class="btn btn-outline-danger">로그아웃</button>`;
+
+    window.sessionStorage.setItem("email", email);
+  }
+});
+
+document.getElementById("productListDiv").addEventListener("click", (e) => {
+  if(e.target.id === "addCart"){
+    axios.post("http://localhost:8080/addCart", {});
+
+
+  }
+});
+
+document.getElementById("loginSpan").addEventListener("click", (e) => {
+  if(e.target.id === "logoutBtn"){
+    window.sessionStorage.removeItem("email");
+    window.location.reload();
+  }
 });
---
+++ 6Week/28Day/README.md
@@ -0,0 +1,33 @@
+Spring(back 서버)와 프론트 서버가 분리되어있을 때
+
+로그인 기능을 구현하고자한다.
+
+그러면 Spring에서 HttpServletRequest를 통해
+
+http 통신 중 해더 부분에 자동으로 session을 함께 보낸다.
+
+그러면 프론트(웹 브라우저)에서 세션과 Spring 서버에서의 세션이 동일하다는 것을 확인할 수 있다.
+
+그리고나서 프론트에서 로그인이 유지된 상태에서 로그인이 살아있냐고 백엔드 서버에 요청을 하면
+
+HttpServletRequest에서 해더 부분에 Session이 존재하지않다고(null) 나온다.
+
+그 이유는 프론트에서 해더부분에 Session을 넣어 보내지않았기 때문이다.
+
+[26일차](https://github.com/seungwoo505/LG-U-PLUS-UREKA-2AN/tree/main/6Week/26Day/project1)와
+ [cafe](https://github.com/seungwoo505/cafe)를 보면 알 수 있듯이 백엔드 서버에서는 CORS를 처리했으나 프론트에서는 CORS처리를 하지않았다.
+
+그렇기에 백엔드에서는 처리를 해주어 정상적으로 프론트에 넘겨 Session을 확인할 수 있었으나
+프론트에서는 처리하지않아 백엔드로 넘길 때 해더에 Session이 없어 null로 나온다.
+
+이것을 해결할 수 있는 방법이 2가지가 존재한다.
+
+1. 하나로 통합하기
+    - 백엔드와 프론트엔드를 하나로 통합을 하는 것이다.
+    - 이러면 CORS 정책에 위배될 일이 없기 때문에 양쪽에서 정상적으로 처리가 될 것이다.
+    - [통합된 서버](https://github.com/seungwoo505/cafe/tree/feat/login)에서 자세히 확인할 수 있다.
+    - 쉽게 말하자면 프론트에 있는 것을 백서버에 함께 등록하면되는 것이다.
+
+2. 프론트단에서도 CORS처리해주기
+    - 백에서 했던 처리를 프론트에서 해주는 것이다.
+    - 자세한 것은 [프론트 처리](https://github.com/seungwoo505/project1)에서 확인할 수 있다.
